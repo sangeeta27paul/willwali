0104853208906ffa528d4375dc14a101a6f3f48b
sangeeta27paul <120769381+sangeeta27paul@users.noreply.github.com>
2023-06-27 17:49:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,63 +2,78 @@ let express = require("express");
 let app = express();
 let ejs = require("ejs");
 const puppeteer = require('puppeteer');
+const bodyParser = require("body-parser");
+// app.use(bodyParser.urlencoded({
+//     extended:true
+// }));
+app.use(bodyParser.json());
+const PORT = process.env.PORT || 5000;
+require('dotenv').config();
 const fs = require('fs');
 // let pdf = require("html-pdf");
 let path = require("path");
-let students = [
-   {name: "Joy",
-    email: "joy@example.com",
-    city: "New York",
-    country: "USA"},
-   {name: "John",
-    email: "John@example.com",
-    city: "San Francisco",
-    country: "USA"},
-   {name: "Clark",
-    email: "Clark@example.com",
-    city: "Seattle",
-    country: "USA"},
-   {name: "Watson",
-    email: "Watson@example.com",
-    city: "Boston",
-    country: "USA"},
-   {name: "Tony",
-    email: "Tony@example.com",
-    city: "Los Angels",
-    country: "USA"
-}];
-app.get("/generateReport", (req, res) => {
-    ejs.renderFile(path.join(__dirname, './views/', "template.ejs"), {students: students}, (err, data) => {
-    if (err) {
-          res.send(err);
-    } else {
-        (async () => {
+const { Console } = require("console");
 
-            // Create a browser instance
-            const browser = await puppeteer.launch();
-          
-            // Create a new page
-            const page = await browser.newPage();
-          
-            //Get HTML content from HTML file
-            const html = fs.readFileSync('template.ejs', 'utf-8');
-            await page.setContent(html, { waitUntil: 'domcontentloaded' });
-          
-            // To reflect CSS used for screens instead of print
-            await page.emulateMediaType('screen');
-          
-            // Downlaod the PDF
-            const pdf = await page.pdf({
-              path: 'result.pdf',
-              margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
-              printBackground: true,
-              format: 'A4',
-            });
-          
-            // Close the browser instance
-            await browser.close();
-          })();
+//console.log(path.join(__dirname, './', "template.ejs"));
+app.post("/generateReport", (req, res) => {
+    let details=[{name:req.body.name,
+        passport:req.body.passport,
+        nationality:req.body.nationality,
+        residance:req.body.residance,
+        assetcountry:req.body.assetcountry 
+    },
+    {executor1:{name:req.body.exe1.name,
+        relation:req.body.exe1.relation,
+        passport:req.body.exe1.passport,
+        contactno:req.body.exe1.contactno,
+        email:req.body.exe1.email
+            },
+    executor2:{name:req.body.exe2.name,
+        relation:req.body.exe2.relation,
+        passport:req.body.exe2.passport,
+        contactno:req.body.exe2.contactno,
+        email:req.body.exe2.email
+            }
+
     }
-});
+    ];
+    let bank={name:req.body.bank.name,
+            bene:req.body.bank.bene
+    };
+   
+
+    
+   // console.log(bank.name);
+    let browser;
+  (async () => {
+    browser = await puppeteer.launch();
+    const [page] = await browser.pages();
+    const html = await ejs.renderFile("template.ejs", {details:details[0],exe:details[1],bank:bank});
+    await page.setContent(html);
+    const pdf = await page.pdf({format: "A4"});
+    res.contentType("application/pdf");
+
+    // optionally:
+    res.setHeader(
+      "Content-Disposition",
+      "attachment; filename=invoice.pdf"
+    );
+
+    res.send(pdf);
+  })()
+    .catch(err => {
+      console.error(err);
+      res.sendStatus(500);
+    }) 
+    .finally(() => browser?.close());
 })
-app.listen(3000);
+
+
+
+
+app.get("/",(req,res)=>{
+  res.send("Server")
+})
+app.listen(PORT,(req,res)=>{
+  console.log(`Server is runnong on port ${PORT}`)
+});
